7027d12722a99d319d92a165498f0ab5d559a61a
usefercas <124436934+usefercas@users.noreply.github.com>
2025-03-22 16:24:18 +0100

    tamaño robot

--- src/components/SplineComponent.jsx
+++ src/components/SplineComponent.jsx
@@ -3,8 +3,18 @@ import Spline from "@splinetool/react-spline";
 
 const RobotComponent = () => {
   return (
-    <div className="w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px]">
-      <Spline scene="https://prod.spline.design/mnxYiodWL6Wc4L0p/scene.splinecode" />
+    <div className="flex justify-center items-center">
+      <div className="w-[600px] h-[600px] sm:w-[300px] sm:h-[300px] relative">
+        <Spline scene="https://prod.spline.design/mnxYiodWL6Wc4L0p/scene.splinecode" />
+        <style>
+          {`
+            canvas {
+              width: 100% !important;
+              height: 100% !important;
+            }
+          `}
+        </style>
+      </div>
     </div>
   );
 };
